8e28ae3060f5ac88a7f7d44fd79b64067622f3d6
longemen3000 <longemen3000@gmail.com>
2020-11-02 16:19:36 -0300

    removed extra code

--- src/base/types.jl
+++ src/base/types.jl
@@ -20,23 +20,3 @@ abstract type SatLiquidModel <: ThermoState.ThermoModel end
 #catch all to build models that doesnt need automatically
 #to stop erroring
 
-
-
-#abstract types corresponding 
-#to certain points on the thermodynamic params space
-
-primitive type MyEnum <: Base.Enums.Enum{Int32} 32 end 
-function MyEnum(x::Integer)
-    1<=x<=3 || Base.Enums.enum_argument_error(MyEnum,x)
-    return Base.bitcast(MyEnum, convert(Int32, x))
-end
-
-Base.Enums.namemap(x::Type{MyEnum}) = Dict{Int32,Symbol}(1=>:a,2=>:b,3=>:c)
-
-Base.typemin(x::Type{MyEnum}) = MyEnum(1)
-Base.typemax(x::Type{MyEnum}) = MyEnum(3)
-
-const a = MyEnum(1)
-const b = MyEnum(2)
-const c = MyEnum(3)
-Base.instances(x::Type{MyEnum}) = (a,b,c)
